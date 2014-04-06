2b58b6d695ef6f7faf39e08a899c00f4293f9f39
Kerem Karatal <kkaratal@yahoo.com>
2014-04-06 14:03:54 -0700

    Demo data for a seating and the new seating model.

--- Classes/CVNSeating.h
+++ Classes/CVNSeating.h
@@ -7,9 +7,16 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Mantle/Mantle.h>
 
-@interface CVNSeating : NSObject
+@interface CVNSeating : MTLModel<MTLJSONSerializing>
 
+@property(nonatomic, strong) NSArray *registeredUsers;
+@property(nonatomic, assign) NSInteger seatingId;
+
++ (void) findBySeatingId:(NSInteger) seatingId
+                 success:(void (^)(CVNSeating *seating))success
+                 failure:(void (^)(NSError *error))failure;
 - (void) seating:(NSString *) seatingId willNotifyWhenUsersCheckin:(void (^)(NSArray *users))usersCheckedIn;
 
 @end
--- Classes/CVNSeating.m
+++ Classes/CVNSeating.m
@@ -12,7 +12,34 @@
 
 #import <Mantle/MTLJSONAdapter.h>
 
+static NSString * const kRegisteredUsers = @"registered_users";
+static NSString * const kSeatingId = @"seating_id";
+
 @implementation CVNSeating
+
++ (NSDictionary *)JSONKeyPathsByPropertyKey {
+  return @{
+           @"registeredUsers": kRegisteredUsers,
+           @"seatingId": kSeatingId
+           };
+}
+
++ (void) findBySeatingId:(NSInteger) seatingId
+                 success:(void (^)(CVNSeating *seating))success
+                 failure:(void (^)(NSError *error))failure {
+  NSDictionary *mockSeatings = [CVNSeating mockSeatings];
+  NSString *seatingIdString = [NSString stringWithFormat:@"%lu", (long) seatingId];
+  
+  NSDictionary *dict = [mockSeatings objectForKey:seatingIdString];
+  NSError *error;
+  CVNSeating *seating = [MTLJSONAdapter modelOfClass:CVNSeating.class fromJSONDictionary:dict error:&error];
+  success(seating);
+}
+
++ (NSValueTransformer *) registeredUsersJSONTransformer {
+  return [NSValueTransformer mtl_JSONArrayTransformerWithModelClass:CVNUser.class];
+}
+
 
 - (void) seating:(NSString *) seatingId willNotifyWhenUsersCheckin:(void (^)(NSArray *users))usersCheckedIn {
   NSArray *users = [self readUsers];
@@ -45,5 +72,35 @@
   return users;
 }
 
++ (NSDictionary *) mockSeatings {
+  NSDictionary *mockSeatings = @{@"46122": @{@"registered_users": @[
+                                            @{@"userId": @"a1b1c1d1e1",
+                                              @"image": @"https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg",
+                                              @"display_name": @"adellecharles"
+                                              },
+                                            @{@"userId": @"a2b1c1d1e1",
+                                              @"image": @"https://s3.amazonaws.com/uifaces/faces/twitter/soffes/128.jpg",
+                                              @"display_name": @"soffes"
+                                              },
+                                            @{@"userId": @"a3b1c1d1e1",
+                                              @"image": @"https://s3.amazonaws.com/uifaces/faces/twitter/divya/128.jpg",
+                                              @"display_name": @"divya"
+                                              }
+                                            ],
+                                             @"seating_id": @(46122)},
+                                 @"44924": @{@"registered_users": @[
+                                            @{@"userId": @"a4b1c1d1e1",
+                                              @"image": @"https://s3.amazonaws.com/uifaces/faces/twitter/walterstephanie/128.jpg",
+                                              @"display_name": @"walterstephanie"
+                                              },
+                                            @{@"userId": @"a5b1c1d1e1",
+                                              @"image": @"https://s3.amazonaws.com/uifaces/faces/twitter/atariboy/128.jpg",
+                                              @"display_name": @"atariboy"
+                                              }
+                                            ],
+                                             @"seating_id": @(44924)}                                             
+                                 };
+  return mockSeatings;
+}
 
 @end
